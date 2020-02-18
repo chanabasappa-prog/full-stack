5cd75ddef4912044f8f4fb7c5d96fc6a8f53da25
chanabasu <cmawati@gmail.com>
2020-02-18 10:57:58 +0200

    Testing Eclipse commit

--- timesheet-app/src/main/java/com/timesheet/TimesheetApplication.java
+++ timesheet-app/src/main/java/com/timesheet/TimesheetApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimesheetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimesheetApplication.class, args);
+		SpringApplication.run(TimesheetApplication.class, args); 
 	}
 
 }
