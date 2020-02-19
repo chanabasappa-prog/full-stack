91976b462b4f9fc0786b2010216b51fd2effdd0a
Khalid Shaikh <shaikh.khalid7@gmail.com>
2020-02-19 16:26:04 +0200

    deleted schema sql
    deleted schema sql

--- personal-details-app/src/main/resources/data.sql
+++ personal-details-app/src/main/resources/data.sql
@@ -1,3 +1,7 @@
+DROP TABLE  IF EXISTS  personaldetail;
+
+CREATE TABLE IF NOT EXISTS personaldetail (id bigint PRIMARY KEY auto_increment, name varchar(255));
+
 insert into personaldetail values (1, 'Home');
 insert into personaldetail values (2, 'Office');
 
--- personal-details-app/src/main/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE  IF EXISTS  personaldetail;
-
-CREATE TABLE IF NOT EXISTS personaldetail (id bigint PRIMARY KEY auto_increment, name varchar(255));
