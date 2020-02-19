6c344944244d0c203e6b0baff8ed3aa2b3531694
chanabasu <cmawati@gmail.com>
2020-02-19 11:15:35 +0200

    Insert data was not working, now fixed it. Created services for create timesheet and delete timesheet.

--- timesheet-app/src/main/resources/data.sql
+++ timesheet-app/src/main/resources/data.sql
@@ -1,4 +1,8 @@
-insert into timesheet(timesheet_date,employee_id,hours) values('10/10/2019',2,8);
-insert into timesheet(timesheet_date,employee_id,hours) values('10/10/2019',2,8);
-insert into timesheet(timesheet_date,employee_id,hours) values('10/10/2019',2,8);
-insert into timesheet(timesheet_date,employee_id,hours) values('10/10/2019',2,8);
+DROP TABLE  IF EXISTS  timesheet;
+
+CREATE TABLE IF NOT EXISTS timesheet (id bigint PRIMARY KEY auto_increment, timesheet_date DATE DEFAULT CURRENT_TIMESTAMP, employee_id bigint, hours bigint);
+
+insert into timesheet(timesheet_date,employee_id,hours) values(CURRENT_TIMESTAMP,2,8);
+insert into timesheet(timesheet_date,employee_id,hours) values(CURRENT_TIMESTAMP,2,8);
+insert into timesheet(timesheet_date,employee_id,hours) values(CURRENT_TIMESTAMP,2,8);
+insert into timesheet(timesheet_date,employee_id,hours) values(CURRENT_TIMESTAMP,2,8);
--- timesheet-app/src/main/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE  IF EXISTS  timesheet;
-
-CREATE TABLE IF NOT EXISTS timesheet (id bigint PRIMARY KEY auto_increment, timesheet_date varchar(255), employee_id bigint, hours bigint);
